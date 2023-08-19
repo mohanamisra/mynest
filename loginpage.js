0752454935f0d7d3b62c144e4b4f89d8465834ec
Sohan Venkatesh <126096232+sohv@users.noreply.github.com>
2023-08-19 14:48:16 +0530

    Update loginpage.js

--- loginpage.js
+++ loginpage.js
@@ -1,32 +1,8 @@
-$(document).ready(function () {
-   $("#login-form").submit(function (e) {
-      e.preventDefault();
-      showPopup("Logged in successfully!");
-   });
-
-   $("#signup-form").submit(function (e) {
-      e.preventDefault();
-      showPopup("Account created successfully!");
-   });
-
-   $(".switch-tab").click(function () {
-      const targetTab = $(this).attr("href");
-      $(".nav-link").removeClass("active");
-      $(targetTab + "-tab").addClass("active");
-      $(".tab-pane").removeClass("show active");
-      $(targetTab).addClass("show active");
-      history.replaceState(null, null, targetTab);
-   });
-
-   function showPopup(message) {
-      const popup = document.createElement("div");
-      popup.className = "custom-popup";
-      popup.textContent = message;
-
-      document.body.appendChild(popup);
-
-      setTimeout(function () {
-         popup.remove();
-      }, 2000);
-   }
+$(".switch-tab").click(function () {
+   const targetTab = $(this).attr("href");
+   $(".nav-link").removeClass("active");
+   $(targetTab + "-tab").addClass("active");
+   $(".tab-pane").removeClass("show active");
+   $(targetTab).addClass("show active");
+   history.replaceState(null, null, targetTab);
 });
