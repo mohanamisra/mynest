78fa734d3304294322b8fbbaec6668634b86d859
Aryan D <aryan.d.dalal@gmail.com>
2023-08-19 15:01:07 +0530

    stuff, data,property html

--- main.js
+++ main.js
@@ -7,22 +7,23 @@ const url = require('url');
 app.use(express.static('public'))
 app.use(express.static('files'))
 
+const CITIES = './files/cities.json';
+
 app.get('/', (req, res) => {
   fs.readFile('./public/index.html', function (err, data) {
     res.send(data);
   });
 });
 app.get('/search-results.html?', (req, res) => {
-  //console.log(req.url);
   var q = url.parse(req.url, true);
   console.log(q.query);
   var search = q.query.search;
-  fs.readFile('./files/cities.json', function (err, data) {
+  fs.readFile(CITIES, function (err, data) {
     if (err) { res.status(404).send('File not found :('); }
     else {
       const loc = JSON.parse(data);
       if (loc.localities.includes(search)) {
-        
+
       } else {
         res.status(404).send("locality not found");
       }
